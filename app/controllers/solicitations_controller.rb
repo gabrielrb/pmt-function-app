0415f19553962a57dc142429b06681544478e419
gabriel belo <gabrielrbelo@gmail.com>
2020-10-28 20:04:38 -0300

    new and create routes and actions created;

--- app/controllers/solicitations_controller.rb
+++ app/controllers/solicitations_controller.rb
@@ -3,7 +3,26 @@ class SolicitationsController < ApplicationController
 
   def show
     @solicitation = Solicitation.find(params[:id])
-    @installment_amount = pmt_function(@solicitation.loan_amount, @solicitation.number_of_installments)
+  end
+
+  def new
+    @solicitation = Solicitation.new
+  end
+
+  def create
+    @solicitation = Solicitation.new(solicitation_params)
+    @solicitation.installment_amount = pmt_function(@solicitation.loan_amount, @solicitation.number_of_installments)
+    if @solicitation.save
+      redirect_to solicitations_path(@solicitation), notice: 'Your solicitation was successfully created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def album_params
+    params.require(:solicitation).permit(:loan_amount, :number_of_installments)
   end
 
   def pmt_function(loan_amount, number_of_installments)
