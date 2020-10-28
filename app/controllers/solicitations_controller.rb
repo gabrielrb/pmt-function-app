581ebe30a5209f0547a17ca5497b2d6a6d375ec2
gabriel belo <gabrielrbelo@gmail.com>
2020-10-28 12:04:01 -0300

    pmt function created (not tested)

--- app/controllers/solicitations_controller.rb
+++ app/controllers/solicitations_controller.rb
@@ -1,5 +1,12 @@
 class SolicitationsController < ApplicationController
+  require "bigdecimal"
+
   def show
     @solicitation = Solicitation.find(params[:id])
+  end
+
+  def pmt_function(loan_amount, number_of_installments)
+    rate = BigDecimal('0.015')
+    loan_amount * ((((1 + rate) ** number_of_installments) X rate) / (((1 + rate) ** number_of_installments) - 1))
   end
 end
