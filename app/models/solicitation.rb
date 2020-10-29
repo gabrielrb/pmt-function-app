f1d72832ce0cf3d79d4e37419be603049c769af7
gabriel belo <gabrielrbelo@gmail.com>
2020-10-29 17:02:40 -0300

    solicitation validations created

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -1,3 +1,13 @@
 class Solicitation < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :loan_amount, presence: true,
+                          numericality: { only_integer: true, message: 'Only numbers' },
+                          length: { maximum: 10 }
+  validates :number_of_installments, presence: true,
+                                     numericality: { only_integer: true, message: 'Only numbers' },
+                                     length: { maximum: 2 }
+  validates :installment_amount, presence: true,
+                                 numericality: { only_integer: true, message: 'Only numbers' }
 end
