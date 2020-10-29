b9db2982a69c5edb9e9d3447df975f55d4a93905
gabriel belo <gabrielrbelo@gmail.com>
2020-10-29 17:53:23 -0300

    users validations created

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -4,10 +4,10 @@ class Solicitation < ApplicationRecord
   validates :user, presence: true
   validates :loan_amount, presence: true,
                           numericality: { only_integer: true, message: 'Only numbers' },
-                          length: { maximum: 10 }
+                          length: { maximum: 10, message: '10 digits max' }
   validates :number_of_installments, presence: true,
                                      numericality: { only_integer: true, message: 'Only numbers' },
-                                     length: { maximum: 2 }
+                                     length: { maximum: 2, message: '2 digits max' }
   validates :installment_amount, presence: true,
                                  numericality: { only_integer: true, message: 'Only numbers' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :solicitations, dependent: :destroy
+
+  validates :name, presence: true,
+                   uniqueness: true
+  validates :cnpj, presence: true,
+                   uniqueness: true,
+                   format: { with: /\A(([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2}))\z/,
+                             message: 'Invalid format' }
+  validates :main_phone_number, presence: true,
+                                uniqueness: true,
+                                format: { with: /\A\(?\b([0-9]{2,3}|0((x|[0-9]){2,3}[0-9]{2}))\)?\s*[0-9]{4,5}[- ]*[0-9]{4}\b/,
+                                          message: 'Invalid format' }
+  validates :secondary_phone_number, uniqueness: true,
+                                     format: { with: /\A\(?\b([0-9]{2,3}|0((x|[0-9]){2,3}[0-9]{2}))\)?\s*[0-9]{4,5}[- ]*[0-9]{4}\b/,
+                                               message: 'Invalid format' }
+  validates :main_address, presence: true,
+                           uniqueness: true
+  validates :secondary_address, uniqueness: true
 end
