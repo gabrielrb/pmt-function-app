74f9f1d473e8d8a5e2c207247a576aaf1902b41a
gabriel belo <gabrielrbelo@gmail.com>
2020-10-30 12:10:28 -0300

    solicitation fictures created and valid test created;

--- test/models/solicitation_test.rb
+++ test/models/solicitation_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class SolicitationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_valid
+    assert solicitations(:one).valid?
+    refute Solicitation.new.valid?
+  end
 end
